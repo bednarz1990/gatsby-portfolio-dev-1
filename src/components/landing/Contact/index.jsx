a2b634540cb3b17c8d9675aa5506b87b17fcddf7
Lukasz Bednarczyk <lukasz.bednarczyk@cdsazure.onmicrosoft.com>
2020-04-17 13:53:19 +0200

    Details updated.

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -10,7 +10,7 @@ export const Contact = () => (
       <ContactForm />
     </Details>
     <Thumbnail>
-      <img src={contact} alt="I’m John and I’m a Backend & Devops engineer!" />
+      <img src={contact} alt="I’m Lukasz and I’m a full-stack software engineer!" />
     </Thumbnail>
   </Wrapper>
 )
