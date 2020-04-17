17a0c4933f77921a21cfef1ec599f9083f620610
Lukasz Bednarczyk <lukasz.bednarczyk@cdsazure.onmicrosoft.com>
2020-04-17 13:36:18 +0200

    Social changes added.

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -10,7 +10,7 @@ export const Intro = () => (
 		<Header />
 		<IntroWrapper as={Container}>
 			<Details>
-				<h1>Hi There!</h1>
+        <h1>Hi There!</h1>
 				<h4>I’m John and I’m a Backend & Devops engineer!</h4>
 				<Button as={AnchorLink} href="#contact">
 					Hire me
